eac9d3c7441e89be45c30043513f7461dfb5d354
ゆあん。 <59510695+yuan-chan@users.noreply.github.com>
2024-01-17 17:03:51 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    name: "yuanchan",
+    image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "Developer, Designer, Univ Stdt",
+    bio: "Doing something everyday. Can speak JPN, a lil bit ENG",
+    email: "me@4nanai.com",
+    linkedin: "",
+    github: "yuan-chan",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `DiscordRedirect`,
+      href: "https://github.com/yuan-chan/discordredirect",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "chil-log",
+    description: "My blog still chilling.",
   },
 
   // CONFIG configration (required)
@@ -53,7 +53,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
